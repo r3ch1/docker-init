181496527837e6a925948506708290e245cd5832
Heric Branco <hericbranco@gmail.com>
2023-11-30 18:47:31 -0300

    feat - identify folder and use this

--- exec.sh
+++ exec.sh
@@ -3,6 +3,9 @@ PHP_VERSION='7.4'
 PROJECT_NAME=$(basename "`pwd`")
 PROJECT_FOLDER=''
 XDEBUG_VERSION='-3.1.0'
+BASEDIR=$(dirname "$0")
+
+echo "$BASEDIR"
 
 read -p "Enter project name [$PROJECT_NAME]: " sRead
 sRead=${sRead:-$PROJECT_NAME}
@@ -30,18 +33,18 @@ echo $PROJECT_NAME
 echo $PROJECT_FOLDER
 echo $PHP_VERSION
 
-cp -R /home/rechi/projetos/personal/docker-init/base /home/rechi/projetos/personal/docker-init/to-export
+cp -R ${BASEDIR}/base ${BASEDIR}/to-export
 
-sed "s!{{project-name}}!${PROJECT_NAME}!ig" /home/rechi/projetos/personal/docker-init/to-export/docker-compose.yml > /home/rechi/projetos/personal/docker-init/to-export/docker-compose-base.yml
-sed "s!{{project-folder}}!${PROJECT_FOLDER}!ig" /home/rechi/projetos/personal/docker-init/to-export/docker-compose-base.yml > /home/rechi/projetos/personal/docker-init/to-export/docker-compose.yml
-rm -rf /home/rechi/projetos/personal/docker-init/to-export/docker-compose-base.yml
+sed "s!{{project-name}}!${PROJECT_NAME}!ig" ${BASEDIR}/to-export/docker-compose.yml > ${BASEDIR}/to-export/docker-compose-base.yml
+sed "s!{{project-folder}}!${PROJECT_FOLDER}!ig" ${BASEDIR}/to-export/docker-compose-base.yml > ${BASEDIR}/to-export/docker-compose.yml
+rm -rf ${BASEDIR}/to-export/docker-compose-base.yml
 
-sed "s!{{php-version}}!${PHP_VERSION}!ig" /home/rechi/projetos/personal/docker-init/to-export/docker/Dockerfile > /home/rechi/projetos/personal/docker-init/to-export/docker/Dockerfile-base
-sed "s!{{xdebug-version}}!${XDEBUG_VERSION}!ig" /home/rechi/projetos/personal/docker-init/to-export/docker/Dockerfile-base > /home/rechi/projetos/personal/docker-init/to-export/docker/Dockerfile
-rm -rf /home/rechi/projetos/personal/docker-init/to-export/docker/Dockerfile-base
+sed "s!{{php-version}}!${PHP_VERSION}!ig" ${BASEDIR}/to-export/docker/Dockerfile > ${BASEDIR}/to-export/docker/Dockerfile-base
+sed "s!{{xdebug-version}}!${XDEBUG_VERSION}!ig" ${BASEDIR}/to-export/docker/Dockerfile-base > ${BASEDIR}/to-export/docker/Dockerfile
+rm -rf ${BASEDIR}/to-export/docker/Dockerfile-base
 
-sed "s!{{project-name}}!${PROJECT_NAME}!ig" /home/rechi/projetos/personal/docker-init/to-export/docker/nginx/nginx.conf > /home/rechi/projetos/personal/docker-init/to-export/docker/nginx/nginx-base.conf
-mv /home/rechi/projetos/personal/docker-init/to-export/docker/nginx/nginx-base.conf /home/rechi/projetos/personal/docker-init/to-export/docker/nginx/nginx.conf
+sed "s!{{project-name}}!${PROJECT_NAME}!ig" ${BASEDIR}/to-export/docker/nginx/nginx.conf > ${BASEDIR}/to-export/docker/nginx/nginx-base.conf
+mv ${BASEDIR}/to-export/docker/nginx/nginx-base.conf ${BASEDIR}/to-export/docker/nginx/nginx.conf
 
-cp -R /home/rechi/projetos/personal/docker-init/to-export/* .
-rm -rf /home/rechi/projetos/personal/docker-init/to-export
+cp -R ${BASEDIR}/to-export/* .
+rm -rf ${BASEDIR}/to-export
